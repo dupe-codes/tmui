69cfdb8025a3310c28bd3e63f57eff6ad0e41599
dupe <dupe@dupe.codes>
2024-04-06 19:55:43 -0400

    feat: ✨ lots of dream learning progress

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 .PHONY: setup
-setup:
+setup: ## Create initial project setup
 	opam switch create . --no-install
 	opam install -y dune utop ocamlformat ocaml-lsp-server
 
 .PHONY: deps
-deps:
+deps: ## Install project dependencies
 	dune build
 	opam install . -y --deps-only --with-test
 
 .PHONY: run
-run:
+run: ## Run the TMUI
 	eval $$(opam env) && dune exec -w otmui
+
+.PHONY: help
+help: ## Show this help
+	@grep -E '^[a-z.A-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
--- bin/otmui/otmui.ml
+++ bin/otmui/otmui.ml
@@ -1,3 +1,7 @@
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+
+type message_object = { message : string } [@@deriving yojson]
+
 let successful = ref 0
 let failed = ref 0
 
@@ -10,16 +14,64 @@ let count_requests inner_handler request =
     failed := !failed + 1;
     raise exn
 
+(*NOTE: json*)
+
 let () =
-  Dream.run ~error_handler:Dream.debug_error_handler
-  @@ Dream.logger
+  Dream.run @@ Dream.logger @@ Dream.origin_referrer_check
   @@ Dream.router
        [
-         Dream.get "/:word" (fun req ->
-             Dream.param req "word" |> Template.render |> Dream.html);
-         (*Dream.get "/bad" (fun _ -> Dream.empty `Bad_Request);*)
-         (*Dream.get "/fail" (fun _ -> raise (Failure "the web app failed!"));*)
+         Dream.post "/" (fun request ->
+             let%lwt body = Dream.body request in
+             let message_object =
+               body |> Yojson.Safe.from_string |> message_object_of_yojson
+             in
+             `String message_object.message |> Yojson.Safe.to_string
+             |> Dream.json);
        ]
+
+(*NOTE: forms*)
+(*let () =*)
+(*Dream.run @@ Dream.logger @@ Dream.memory_sessions*)
+(*@@ Dream.router*)
+(*[*)
+(*Dream.get "/" (fun request -> Dream.html (Form.show_form request));*)
+(*Dream.post "/" (fun request ->*)
+(*match%lwt Dream.form request with*)
+(*| `Ok [ ("message", message) ] ->*)
+(*Dream.html (Form.show_form ~message request)*)
+(*| _ -> Dream.empty `Bad_Request);*)
+(*]*)
+
+(*NOTE: cookies*)
+(*let () =*)
+(*Dream.run @@ Dream.set_secret "foo" @@ Dream.logger*)
+(*@@ fun request ->*)
+(*match Dream.cookie request "ui.language" with*)
+(*| Some value ->*)
+(*Printf.ksprintf Dream.html "Your preferred language is %s!"*)
+(*(Dream.html_escape value)*)
+(*| None ->*)
+(*let response = Dream.response "Set language preference; come again!" in*)
+(*Dream.add_header response "Content-Type" Dream.text_html;*)
+(*Dream.set_cookie response request "ui.language" "ut-OP";*)
+(*Lwt.return response*)
+
+(*NOTE: Sessions*)
+(*let () =*)
+(*Dream.run @@ Dream.logger @@ Dream.memory_sessions*)
+(*@@ fun request ->*)
+(*match Dream.session_field request "user" with*)
+(*| None ->*)
+(*let%lwt () = Dream.invalidate_session request in*)
+(*let%lwt () = Dream.set_session_field request "user" "alice" in*)
+(*Dream.html "You weren't logged in; but now you are!"*)
+(*| Some username ->*)
+(*Printf.kprintf Dream.html "Welcome back %s" (Dream.html_escape username)*)
+
+(*let () =*)
+(*Dream.run*)
+(*~error_handler:(Dream.error_template Error_template.my_error_template)*)
+(*@@ Dream.logger @@ Dream.not_found*)
 
 (*let () =*)
 (*Dream.run @@ Dream.logger @@ count_requests*)
